8df15653c00adbcbd03915121ad3dbd717e6a81f
Seong <pok_gare@naver.com>
2020-03-26 15:31:12 +0900

    feat(fe+be) 서버와 클라이언트 파일 추가

---
+++ server/src/main/java/me/sjh2428/daitnus2/BoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package me.sjh2428.daitnus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerPlateApplication.class, args);
+	}
+
+}
